f97832fb6db9a8b5a14466a7b33979ec349dc9e7
EC2 Default User <ec2-user@ip-172-31-62-181.ec2.internal>
2016-03-23 16:37:08 +0000

    added some logging

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var restify = require('restify');
 var server = restify.createServer();
 server.use(restify.queryParser());
 
-
 server.get("/", function (req, res, next){
-  res.send(200, {result:"i'm server #1"})
+  console.log("got GET request");
+  res.send(200, {result:"i'm server #1"});
 })
 
 server.listen(8080, function() {
