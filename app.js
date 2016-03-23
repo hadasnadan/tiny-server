872c27e502950175d70a13dbc99d77191cb8478a
EC2 Default User <ec2-user@ip-172-31-62-181.ec2.internal>
2016-03-23 16:58:05 +0000

    improved logging

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var restify = require('restify');
+var moment = require('moment');
 
 var server = restify.createServer();
 server.use(restify.queryParser());
 
 server.get("/", function (req, res, next){
-  console.log("got GET request");
+  console.log("got GET request at "+ moment().format('h:mm:ss'));
+
+  res.header("Cache-Control","max-age=3600");
+
   res.send(200, {result:"i'm server #1"});
 })
 
