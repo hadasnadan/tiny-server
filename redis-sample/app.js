11b2b5fe18de47f5929f9466aa7e1408143c4942
sharondagan <sharon@digitalcrafts.co>
2016-05-03 15:38:35 +0300

    loaderIO template

--- redis-sample/app.js
+++ redis-sample/app.js
@@ -14,22 +14,6 @@ server.get("/", function (req, res, next){
     res.send(200, {result:"ok"});
 })
 
-server.get("/loaderio-6c90ecae45950a945eaa8652c84d0528.txt", function (req, res, next){
-    res.write("loaderio-6c90ecae45950a945eaa8652c84d0528");
-    res.end();
-}
-
-server.get("/tests", function (req, res, next){
-    res.send(200, {
-            "keys": ["number"] ,
-            "values": [
-                [0], [1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11], [12], [13], [14], [15], [16], [17], [18], [19], [20], [21]
-            ]
-        }
-    );
-})
-
-
 server.get("/fibonacci/:n", function (req, res, next){
     console.log("got GET request at "+ moment().format('h:mm:ss') + " for number "+req.params.n);
 
@@ -58,6 +42,20 @@ function nthFibonacci(n) {
         : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 }
 
+server.get("/loaderio-6c90ecae45950a945eaa8652c84d0528.txt", function (req, res, next){
+    res.write("loaderio-6c90ecae45950a945eaa8652c84d0528");
+    res.end();
+}
+
+server.get("/tests", function (req, res, next){
+    res.send(200, {
+            "keys": ["number"] ,
+            "values": [
+                [0], [1], [2], [3], [4], [5], [6], [7], [8], [9], [10], [11], [12], [13], [14], [15], [16], [17], [18], [19], [20], [21]
+            ]
+        }
+    );
+})
 
 server.listen(8080, function() {
     console.log('%s listening at %s', server.name, server.url);
